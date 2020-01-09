31acb10063c7019bd1395f806984f825d83005d2
Jacob Canote <jacob@webdogs.com>
2020-01-08 17:06:42 -0800

    Update helpers.php

--- inc/helpers.php
+++ inc/helpers.php
@@ -5,14 +5,15 @@ if (!function_exists('array_md5')) {
      * Return the md5 hash string for multi-dimensional array.
      *
      * @param	array	$array
+     * @param	string	$prepend_string	additional string to prepend for md5 hash
      * @param	string	$append_string	additional string to append for md5 hash
      * @param	boolean	$use_serialize	option toString method using serialize
      * @return	string	md5 hash
      */
-    function array_md5(array $array, string $append_string  = '', bool $use_serialize = false)
+    function array_md5(array $array, string $prepend_string  = '', string $append_string  = '', bool $use_serialize = false)
     {
         array_multisort($array);
         $array_string = $use_serialize ? serialize($array) : json_encode($array);
-        return md5($array_string . $append_string);
+        return md5($prepend_string . $array_string . $append_string);
     }
 }
